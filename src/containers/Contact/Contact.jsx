fd67e4ddad5242dc30be54b924636b70e6691d06
ekrahl <59214630+ekrahl@users.noreply.github.com>
2022-11-20 19:41:54 -0500

    refactor

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -8,18 +8,18 @@ export default function Contact() {
         <div className='location-bg flex__center'>
             <div className='location-wrapper flex__center'>
                 <div className='spacer'></div>
-                <h1 className='location-title'>Location</h1>
-                <p className='location-info'><a href='https://www.google.com/maps/place/Taco+Too/@38.9193517,-77.2343642,15z/data=!4m5!3m4!1s0x0:0x5d4e6a1a69279fad!8m2!3d38.9193621!4d-77.2343422'>2000 Chain Bridge Road <br /> Vienna, Virginia, 22182</a></p>
+                <h1 className='title'>Location</h1>
+                <p className='info'><a href='https://www.google.com/maps/place/Taco+Too/@38.9193517,-77.2343642,15z/data=!4m5!3m4!1s0x0:0x5d4e6a1a69279fad!8m2!3d38.9193621!4d-77.2343422'>2000 Chain Bridge Road <br /> Vienna, Virginia, 22182</a></p>
             </div>
             <div className='hours-wrapper flex__Center'>
-                <h1 className='hours-title'>Hours</h1>
-                <p className='hours-info'>Sunday - Friday 7am - 6pm</p>
-                <p className='hours-info'>Saturday 7am - 5pm</p>
+                <h1 className='title'>Hours</h1>
+                <p className='info'>Sunday - Friday 7am - 6pm</p>
+                <p className='info'>Saturday 7am - 5pm</p>
             </div>
             <div className='contact-wrapper flex__center' id='contact'>
-                <h1 className='contact-title'>Contact</h1>
-                <p className='contact-info'><AiOutlinePhone />  571-487-7634</p>
-                <p className='contact-info'><AiOutlineMail />  letstacoboutit@ilovetacotoo.com</p>
+                <h1 className='title'>Contact</h1>
+                <p className='info'><AiOutlinePhone />  571-487-7634</p>
+                <p className='info'><AiOutlineMail />  letstacoboutit@ilovetacotoo.com</p>
             </div>
         </div>
 
